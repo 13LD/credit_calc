5caa626d3fb89bd649ef1720ae600ce7994f48de
Dmytro Lysohor <tompla96@ukr.net>
2018-05-04 15:35:34 +0300

    input validation

--- lib/credit_calc.rb
+++ lib/credit_calc.rb
@@ -2,6 +2,7 @@ class CreditCalc
   attr_reader :payment, :month_percent, :all_payment, :credit, :percent
 
   def initialize(percent, credit, term)
+    raise(ArgumentError, 'No negative params') unless percent.to_f >= 0 and credit.to_f >= 0 and term.to_i > 0
     @percent = percent.to_f
     @credit = [credit.to_f]
     @term = term.to_i
